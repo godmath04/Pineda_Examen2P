f652e7fbe2cfa6d380b7ed553032bafa497fbc09
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2023-12-06 15:45:15 -0500

    Pocos datos para probar el codigo y se agrego la suma de notas y promedio en cada progreso

--- examen.c
+++ examen.c
@@ -6,26 +6,25 @@ Cada semestre, a la materia de Programación se inscriben una cantidad constante
 */
 
 /* CONSIDERACIONES PERSONALES ---  LA GENERACION DE NOTAS SERA ALEATORIA
-LA NOTA SERA SOBRE 100 PUNTOS Y CADA ALUMNO TEDRÁ 5 NOTAS PARA LUEGO OBTENER SU PROMEDIO*/
+LA NOTA SERA SOBRE 0- 100 PUNTOS Y CADA ALUMNO TEDRÁ 5 NOTAS PARA LUEGO OBTENER SU PROMEDIO*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-// Variables globales de entrada que se mantienen en todo el código
 
 // Creo una matriz 3D para que la profundidad indique el progreso, la fila los alumnos y la columna las notas
 void notasProgreso(int progreso, int alumnos, int notas, int MatrizCalificaciones[progreso][alumnos][notas])
 {
     for (int k = 0; k < progreso; k++)
     {
+        printf("***PROGRESO %d:\n", k + 1);
 
         for (int i = 0; i < alumnos; i++)
         {
             printf("Alumno %d:\t", i + 1);
             for (int j = 0; j < notas; j++)
             {
-                MatrizCalificaciones[k][i][j] = rand() % 100 + 1;
+                MatrizCalificaciones[k][i][j] = rand() % 11;
                 printf("%6d\t|", MatrizCalificaciones[k][i][j]);
             }
             printf("\n");
@@ -34,13 +33,38 @@ void notasProgreso(int progreso, int alumnos, int notas, int MatrizCalificacione
     }
 }
 
+void PromedioCalificaciones(int progreso, int alumnos, int notas, int MatrizCalificaciones[progreso][alumnos][notas])
+{
+
+    notasProgreso(progreso, alumnos, notas, MatrizCalificaciones);
+    // SUMA DE NOTAS POR CADA PROGRESO
+    for (int k = 0; k < progreso; k++)
+    {
+        printf("***PROGRESO %d:\n", k + 1);
+
+        for (int i = 0; i < alumnos; i++)
+        {
+            int suma = 0;
+            for (int j = 0; j < notas; j++)
+            {
+
+                suma = suma + MatrizCalificaciones[k][i][j];
+            }
+            float promedio = (float )suma / notas;
+
+            printf("Alumno: %d\t su promedio es: %f\n", i + 1, promedio);
+            printf("\n");
+        };
+    }
+}
+
 int main(int argc, char const *argv[])
 {
 
     printf("***** NOTAS DE LA CARRERA DE ING.*****\n");
-    int alumnos = 23, progreso = 3, notas = 5;
+    int alumnos = 3, progreso = 1, notas = 5;
     int MatrizNotas[progreso][alumnos][notas];
-    notasProgreso(progreso, alumnos, notas, MatrizNotas);
+    PromedioCalificaciones(progreso, alumnos, notas, MatrizNotas);
 
     return 0;
 }
